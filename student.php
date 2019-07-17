0f5bbada65d8e187af6eddbe4c1bdd5970f59ca2
mozhitian <947063856@qq.com>
2019-07-17 17:26:10 +0800

    学生信息添加
    2019年7月17日17:26:06

---
+++ student.php
@@ -0,0 +1,25 @@
+<?php
+	require("conn.php");
+	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求
+	$ret_data = '';
+	$ret_data["success"] = 'success';
+	$name = isset($_POST["name"])?$_POST["name"] : '';
+	$province = isset($_POST["province"])?$_POST["province"] : '';
+	$num = isset($_POST["num"])?$_POST["num"] : '';
+	$sex = isset($_POST["sex"])?$_POST["sex"] : '';
+	$message = isset($_POST["message"])?$_POST["message"] : '';
+	$xueyuan = isset($_POST["xueyuan"])?$_POST["xueyuan"] : '';
+	$dorm = isset($_POST["dorm"])?$_POST["dorm"] : '';
+	$zy = isset($_POST["zy"])?$_POST["zy"] : '';
+	$address = isset($_POST["address"])?$_POST["address"] : '';
+	$code = isset($_POST["code"])?$_POST["code"] : '';
+	$phone = isset($_POST["phone"])?$_POST["phone"] : '';
+	$receive = isset($_POST["receive"])?$_POST["receive"] : '';
+	$result = isset($_POST["result"])?$_POST["result"] : '';
+//	 $time = time();
+		$sql = "INSERT INTO 学生信息 (姓名,省份,考生号,性别,身份证号,二级学院,宿舍号,录取专业,邮寄地址,邮政编码,联系电话,收件人,投档成绩) VALUES ('$name','$province','$num','$sex','$message','$xueyuan','$dorm','$zy','$address','$code','$phone','$receive','$result')";
+		$res = $conn->query($sql);
+    $conn->close();
+	$json=json_encode($ret_data);
+	echo $json
+?>
